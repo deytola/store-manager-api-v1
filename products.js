65b7ffde5e576bca1060347857bc69e4f8e9bb4a
AdetolaAdebola <36213634+deytola@users.noreply.github.com>
2018-10-20 10:46:42 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 const router = express.Router();
+// const app = express();
 const products = [
   {
     id: 1,
@@ -24,6 +25,7 @@ const products = [
     minimumQuantityAllowed: 20,
   },
 ];
+
 
 // Route to get all the products
 router.get('/', (req, res) => {
@@ -49,7 +51,7 @@ router.get('/:id', (req, res) => {
 });
 
 // Route to post a product
-router.post('/', (req, res) => {
+router.post('/add', (req, res) => {
   // Check if all fields are provided and are valid:
   if (!req.body.itemName
        || !req.body.category
@@ -71,12 +73,12 @@ router.post('/', (req, res) => {
 });
 
 // Route to update a product
-router.put('/:id', (req, res) => {
+router.put('/update/:id', (req, res) => {
   // Check if all fields are provided and are valid:
   if (!req.body.itemName
-       || !req.body.category
-        || req.body.price <= 0
-       || !req.params.id.toString().match(/^[0-9]{3,}$/g)) {
+    || !req.body.category
+    || req.body.price <= 0
+    || !req.params.id) {
     res.status(400);
     res.json({ message: 'Bad Request' });
   } else {
@@ -111,10 +113,10 @@ router.put('/:id', (req, res) => {
 });
 
 // Route to delete a product
-router.delete('/:id', (req, res) => {
+router.delete('/delete/:id', (req, res) => {
   const removeIndex = products.map(
     product => product.id,
-  ).indexOf(req.params.id); // Get the index of product with given id.
+  ).indexOf(parseInt(req.params.id, 10)); // Get the index of product with given id.
   if (removeIndex === -1) {
     res.json({ message: 'Product not found' });
   } else {
